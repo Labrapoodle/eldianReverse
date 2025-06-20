cf9405529ec89c576a74645ca99ffd30d630440e
Kirill Alei <aleykirill@gmail.com>
2025-06-21 00:35:26 +0700

    No need to print column now. Filename is enough. Works well on all template files

--- eldianReverse.py
+++ eldianReverse.py
@@ -1,13 +1,14 @@
 import csv
 import sys
 
-filename=sys.argv[2]
+filename=sys.argv[1]
 column_to_read=int(sys.argv[1])-1
 row_to_start=0
 
-if(column_to_read==4):
+if("aliases" in filename ):
     delim=';'
     row_to_start=1
+    column_to_read==4
 else:
     delim=","
 
@@ -23,50 +24,30 @@ except PermissionError:
     sys.exit()
 except IOError as e:
     print(f"\033[31mОшибка ввода-вывода: {e}")
-    sys.exit()
+    sys.exit()   
 
 
-
-    
-
-
-for i in range(row_to_start,len(rows)):
-    
-    
-    
+for i in range(row_to_start,len(rows)):   
     try:
-        if(rows[i][column_to_read]!=""):
-                #if(len(rows[i][column_to_read])!=''):
-                #    ind_in_str=-1
-                #    for k in range(column_to_read):
-                #        ind_in_str=rows[i][0].find(';',ind_in_str+1)
-                #    end_ind = rows[i][0].find(';',ind_in_str+1)
-                #    str_temp = rows[i][0][ind_in_str+2:end_ind-1]
+        if(rows[i][column_to_read]!=""):                
                     strCutZeroInBegin = str(hex(int(rows[i][column_to_read],base=16)))[2:]
                     if(len(strCutZeroInBegin)%2):
                         strCutZeroInBegin="0"+strCutZeroInBegin
                     string=f'{bytes.fromhex(strCutZeroInBegin)[::-1].hex()}'
-                    rows[i][column_to_read]='\t'+string
-
-                    #rows[i].append(f"\"{bytes.fromhex(rows[i][column_to_read])[::-1].hex()}\"")
+                    rows[i][column_to_read]=string
     except ValueError as e:
             print(f'puk  {e} {rows[i][column_to_read]} {type(rows[i][column_to_read])} {strCutZeroInBegin}')
             sys.exit()
     except IndexError:
             print(len(rows[i]),rows[i])
     
-
-print(rows)
 # Перезаписываем файл
 with open(filename, 'w', encoding='utf-8',newline='') as file:
     writer = csv.writer(file, delimiter=delim)
-    #writer.writerows(rows)
     for row in rows:
         writer.writerow([str(cell) for cell in row])         
         
     
-
-
 
 
 
